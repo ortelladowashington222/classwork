19a431ea8ca2d7bb4d0ca4e39f4317f89cbe864d
ortelladowashington222 <ortelladowashington222@gmail.com>
2025-03-09 22:34:11 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int age = 25;
+    char name[] = "John";
+    printf("Hello, my name is %s and I am %d years old.\n", name, age);
+    return 0;
+}
