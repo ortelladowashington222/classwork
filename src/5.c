1c19832dafe2ce6b4834f2f066ff7320f0ec4e36
ortelladowashington222 <ortelladowashington222@gmail.com>
2025-03-11 00:13:24 +0000

    Add 5.c

---
+++ src/5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    if (age >= 18) {
+        printf("You are old enough to drink alcohol.\n");
+    } else {
+        printf("Sorry, you are not old enough to drink alcohol yet.\n");
+    }
+    return 0;
+}
