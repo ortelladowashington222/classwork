1e1580f8f825e4541334ed44c4973f6efe44eb06
ortelladowashington222 <ortelladowashington222@gmail.com>
2025-03-13 21:47:08 +0000

    Add 7.c

---
+++ src/7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int age = 30;
+  if (age > 25) {
+    printf("You are eligible to vote.\n");
+  } else {
+    printf("Sorry, you are not eligible to vote yet.");
+  }
+  return 0;
+}
