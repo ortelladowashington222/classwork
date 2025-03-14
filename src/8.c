df1c7753f78485d5288c3c601cb1946583eb34e2
ortelladowashington222 <ortelladowashington222@gmail.com>
2025-03-14 20:16:08 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    if (num1 > num2) {
+        printf("Number one is greater than number two\n");
+    } else {
+        printf("Number one is less than or equal to number two\n");
+    }
+    return 0;
+}
