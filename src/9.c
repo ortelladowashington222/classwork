f2669ac730b2030618d9f729660c497847cf0fbf
ortelladowashington222 <ortelladowashington222@gmail.com>
2025-03-15 07:05:59 +0000

    Add 9.c

---
+++ src/9.c
@@ -0,0 +1,13 @@
+
+int main(void) {
+  int num1 = 5;
+  float num2 = 3.14;
+
+  if (num1 > num2) {
+    printf("Number 1 is greater than number 2\n");
+  } else {
+    printf("Number 2 is greater than or equal to number 1\n");
+  }
+
+  return 0;
+}
