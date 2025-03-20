4c07c0991ac246b7c40bf639a6e8e68c97201e10
ortelladowashington222 <ortelladowashington222@gmail.com>
2025-03-20 05:13:52 +0000

    Add 14.c

---
+++ src/14.c
@@ -0,0 +1,11 @@
+
+int main() {
+    int x = 5;
+    int y = 10;
+    if (x > y) {
+        printf("X is greater than Y\n");
+    } else {
+        printf("Y is greater than X\n");
+    }
+    return 0;
+}
